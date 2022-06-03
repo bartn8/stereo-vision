1180045fe560478e5c441e75202cc899fe90ec3d
Luca Bartolomei <bartn8@hotmail.it>
2022-06-03 21:22:44 +0200

    test

--- reconstruction/base/rSGM/test.py
+++ reconstruction/base/rSGM/test.py
@@ -4,13 +4,44 @@ from pyrSGM import census5x5_SSE, median3x3_SSE, costMeasureCensus5x5_xyd_SSE, m
 import os
 import time
 
+def rsgm_double_crop(img):
+    _,w = img.shape[:2]
+    if w % 16 != 0:
+        c = w % 16
+        cl = c // 2
+        cr = c - cl
+        img = img[:, cl:w-cr]
+    return img
 
-left = cv2.imread(os.path.join("test_data", "view1_crop.png"), cv2.IMREAD_GRAYSCALE)
-right = cv2.imread(os.path.join("test_data", "view5_crop.png"), cv2.IMREAD_GRAYSCALE)
+def gt_resize_scaling(left, right, scale_factor):
+    h,w = left.shape[:2]
+    left = cv2.resize(left, (w//scale_factor,h//scale_factor))
+
+    h,w = right.shape[:2]
+    right = cv2.resize(right, (w//scale_factor,h//scale_factor))
+
+
+    return left, right
+
+def my_linear_contrast_stretching(gray_image):  
+        pmin,pmax = np.min(gray_image), np.max(gray_image)
+        image = gray_image.copy().astype(np.float32)
+        image -= pmin
+        image *= 255.0 / (pmax-pmin)
+        image = np.clip(image, 0, 255)
+        return image.astype(np.uint8)
+
+left = (cv2.imread(os.path.join("test_data", "im0.png"), cv2.IMREAD_GRAYSCALE))
+right = (cv2.imread(os.path.join("test_data", "im1.png"), cv2.IMREAD_GRAYSCALE))
+
+left, right = gt_resize_scaling(left, right, 1)
+
+left = rsgm_double_crop(left)
+right = rsgm_double_crop(right)
 
 h,w = left.shape[:2]
 h,w = int(h), int(w)
-dispCount = int(128)
+dispCount = int(256)
 
 startTime = time.time()
 
@@ -28,7 +59,7 @@ costMeasureCensus5x5_xyd_SSE(leftCensus, rightCensus, dsi, w, h, dispCount, 4)
 
 dsiAgg = np.zeros((h,w,dispCount), dtype=np.uint16)
 
-aggregate_SSE(left, dsi, dsiAgg, w, h, dispCount, 7, 17, 0.25, 50)
+aggregate_SSE(left, dsi, dsiAgg, w, h, dispCount, 11, 17, 0.5, 35)
 
 dispImg = np.zeros((h,w), dtype=np.float32)
 
@@ -38,8 +69,15 @@ subPixelRefine(dsiAgg, dispImg, w,h,dispCount,0)
 dispImgfiltered = np.zeros((h,w), dtype=np.float32)
 median3x3_SSE(dispImg, dispImgfiltered, w, h)
 
+dispImgfiltered[dispImgfiltered<=0] = 0
+
 print((time.time()-startTime)*1000)
+
+
+dmap_strech = my_linear_contrast_stretching(dispImgfiltered)
+dmaprgb = cv2.applyColorMap(dmap_strech, cv2.COLORMAP_VIRIDIS)
+
 
 cv2.imwrite(os.path.join("test_data", "census1.png"), leftCensus.astype(np.uint8))
 cv2.imwrite(os.path.join("test_data", "census5.png"), rightCensus.astype(np.uint8))
-cv2.imwrite(os.path.join("test_data", "disp1.png"), dispImgfiltered.astype(np.uint8))
+cv2.imwrite(os.path.join("test_data", "disp1.png"), dmaprgb)
